5270260b21e1f5adbc09a91d4f5f240b1248cac2
Misha-Belov <59139007+Misha-Belov@users.noreply.github.com>
2023-09-29 14:19:44 +0300

    4

--- 4.cpp
+++ 4.cpp
@@ -1,25 +1,25 @@
 #include "4.h"
 
-int EPS = 1e-9;
-
+double EPS = 1e-9;
+//
 int check (int K, float resistance[], float resistance_final);
 bool is_zero(float x);
-
+//
 int check (int K, float resistance[], float resistance_final)
 {
-    float d = 0
+    float d = 0;
 
     for(int i = 0; i < K; i++)
     {
         d += resistance[i] - resistance_final;
     }
 
-    if(is_zero(d / k))
+    if(is_zero(d / (float)K))
         return 1;
     return 0;
 }
 
 bool is_zero(float x)
 {
-    return (fabs (a - b) < EPS);
+    return ((double)fabs(x) < EPS);
 }
